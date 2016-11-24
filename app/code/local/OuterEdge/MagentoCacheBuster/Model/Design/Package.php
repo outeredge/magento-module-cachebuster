f8b4c5d2e6df9d52edd4ef9fcb71fcdfaf9c1357
Michael Windell <mike@outeredgeuk.com>
2016-11-24 11:48:05 +0000

    CDN Paths Working

--- app/code/local/OuterEdge/MagentoCacheBuster/Model/Design/Package.php
+++ app/code/local/OuterEdge/MagentoCacheBuster/Model/Design/Package.php
@@ -34,7 +34,7 @@ class OuterEdge_MagentoCacheBuster_Model_Design_Package extends MiddleManClass
             if (!file_exists($path)) {
                 $this->hashes[$url] = $url;
             } else {
-                $this->hashes[$url] = substr_replace($url, '.' . md5_file(ltrim(parse_url($url, PHP_URL_PATH), '/')), strrpos($url, '.'), 0);
+                $this->hashes[$url] = substr_replace($url, '.' . md5_file($path), strrpos($url, '.'), 0);
             }
 
             file_put_contents($this->hashfile, '<?php return ' . var_export($this->hashes, true) . ';');
